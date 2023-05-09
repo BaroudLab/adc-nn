02df844a71bc60d3f089ca4c51b696a90ab168c9
Andrey Aristov <aaristov@pasteur.fr>
2023-05-09 12:15:57 +0200

    add training funcs

--- src/adc_nn/tools/training.py
+++ src/adc_nn/tools/training.py
@@ -1,23 +1,37 @@
+"""
+This module halp to prepare the data for training: 
+retrieveing images and making vectors with features
+"""
+
 from . import io
 import numpy as np
 
 
 def get_unique_records():
+    """
+    Retrieving base64 images of the droplets
+    """
     unique_records = [io.retrieve_random_droplet(c, d)["rgb_image"]
         for c, d in io.readdb("""
             SELECT DISTINCT 
-            droplets.chip_id, 
-            droplets.droplet_id, 
+            chip_id, 
+            droplet_id
             FROM droplets;
         """, unique=False)]
     return unique_records
 
 
 def get_vector_size():
+    """
+    Getting total number of unique features 
+    """
     return io.readdb("SELECT COUNT(*) FROM features;")[0]
 
 
 def get_vectors(features: dict):
+    """
+    Adds vectors to feature list
+    """
     fs = get_vector_size()
     return [{"vector": features_to_vector(fs, rec["features"]), **rec} 
             for rec in features
@@ -25,6 +39,9 @@ def get_vectors(features: dict):
 
 
 def features_to_vector(vector_size: int, features: list):
+    """
+    Converts list of features to the vector of 0 and 1.
+    """
     vector = np.zeros((vector_size, ))
     for f in features:
         vector[f["id"]-1] = 1
